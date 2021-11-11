e17df0cde9b0a2b238cc6c7be68dbb73f204f13b
amane-uehara <62415046+amane-uehara@users.noreply.github.com>
2021-11-12 02:17:37 +0900

    adjust memory size of sifive-e hello world

--- qemu-sifive-e-rv32i/mmode-uart-helloworld/start.s
+++ qemu-sifive-e-rv32i/mmode-uart-helloworld/start.s
@@ -1,25 +1,19 @@
 .GLOBAL _start
 .GLOBAL main
-.GLOBAL _stack_end
-.EQU STACK_SIZE, 1024
 
 .SECTION .text
 
 _start:
-  # initialize stack pointer
-  la          sp, _stack_end + STACK_SIZE
+  # stop all hart except hart0
+  csrr  t0      , mhartid
+  bnez  t0      , _wait_for_intr
 
-  jal         main
-  j           _wait_for_intr
+  # initialize stack pointer
+  la    sp      , _init_stack_ptr
+
+  jal             main
+  j               _wait_for_intr
 
 _wait_for_intr:
   wfi
-  j           _wait_for_intr
-
-
-
-.SECTION .bss
-.ALIGN 4
-
-_stack_end:
-  .SKIP STACK_SIZE
+  j               _wait_for_intr
